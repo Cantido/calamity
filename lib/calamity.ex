52f418ac352d2a0744d07adc1d76f1f1a5e5276c
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-08-07 12:56:07 -0600

    Remove logging statements

--- lib/calamity.ex
+++ lib/calamity.ex
@@ -7,10 +7,7 @@ defmodule Calamity do
   alias Calamity.Command
   alias Calamity.EventStore
 
-  require Logger
-
   def dispatch(command, aggregates, process_managers, event_store) do
-    Logger.info("dispatching command #{inspect command}")
     {agg_mod, agg_id} = Command.aggregate(command)
 
     aggregate = Map.get(aggregates, agg_id, struct!(agg_mod))
@@ -21,8 +18,6 @@ defmodule Calamity do
 
     new_aggregate = Enum.reduce(events, aggregate, &Aggregate.apply(&2, &1))
 
-
-    Logger.info("Resulting events: #{inspect events}")
 
     %{pm_map: new_process_managers, commands: new_commands} =
       Enum.reduce(events, %{pm_map: %{}, commands: []}, fn event, results_map ->
@@ -36,8 +31,6 @@ defmodule Calamity do
           }
         end)
       end)
-
-    Logger.info("Commands: #{inspect new_commands}")
 
     new_aggregates = Map.put(aggregates, agg_id, new_aggregate)
     event_store = Enum.reduce(events, event_store, &EventStore.append(&2, &1))
