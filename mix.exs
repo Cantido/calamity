c4307d09fe520bb72cec89e20fce5aa644dfe9f8
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-08-08 11:38:25 -0600

    Remove the task supervisor
    I don't need it anymore

--- mix.exs
+++ mix.exs
@@ -18,7 +18,6 @@ defmodule Calamity.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {Calamity.Application, []},
       extra_applications: [:logger]
     ]
   end
