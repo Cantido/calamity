f9b8953f9d197ad3847091526f8414f7e8ac860d
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-07 10:46:50 -0600

    Add CI, and a Credo CI step

--- lib/calamity/aggregate/base.ex
+++ lib/calamity/aggregate/base.ex
@@ -1,3 +1,16 @@
 defmodule Calamity.Aggregate.Base do
+  @moduledoc """
+  The base module for any aggregate.
+  Calamity needs a factory function to build new aggregates when one does not exist.
+  This module provides a behaviour to make sure it will work as expected when you hand it to `Calamity`.
+
+  Do not use this module directly, import it with `use Calamity, :aggregate`.
+  """
+
+  @doc """
+  Build a new aggregate struct.
+
+  This function will be called with the ID result of `Calamity.Command.aggregate/1`.
+  """
   @callback new(any()) :: Calamity.Aggregate.t()
 end
--- lib/calamity/process_manager/base.ex
+++ lib/calamity/process_manager/base.ex
@@ -1,5 +1,20 @@
 defmodule Calamity.ProcessManager.Base do
+  @moduledoc """
+  Base module for Calamity's process managers.
+
+  Calamity needs to know if a process manager should be called for a given event,
+  and which process manager to call.
+  Pass a module implementing this behavior to `Calamity.dispatch/5`.
+  """
+
+  @doc """
+  Control whether a process manager should be called for this event.
+  """
   @callback interested?(any()) :: {:start, any()} | {:continue, any()} | {:stop, any()}
+
+  @doc """
+  Create a new process manager with the given ID.
+  """
   @callback new(any()) :: Calamity.ProcessManager.t()
 
   def handle_event(mod, pms, event) do
